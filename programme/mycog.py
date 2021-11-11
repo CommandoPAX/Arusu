00934c45744d183013304bf309b505339ce23d80
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2021-11-12 00:36:32 +0100

    Update mycog.py

--- programme/mycog.py
+++ programme/mycog.py
@@ -2,7 +2,7 @@ from redbot.core import commands
 
 Programme = "1. Bannir Pepsire" \n "2. Réduire la présence des fumos sur le serveur" \n "3. Tout le monde devra tirer une carte du deck chaque jour" \n "4. Le salaire quotidien sera augmenté de 5 Arucoins." \n "5. Cette augmentation sera financé en taxant K.N.G." \n "6. La Corée du Nord sera envahie et éradiquée pour la gloire d'Arusu" \n "7. Des cours de fuséologie seront accessibles à tout le monde et ce gratuitement." \n "8. Le only-fan de Janirf sera en accès libre pour l'intégralité des membres du LSMB."  
 
-class Deck(commands.Cog):
+class Programme(commands.Cog):
     """Programme présidentielle"""
 
     def __init__(self, bot):
