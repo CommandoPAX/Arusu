d2a528a987b273639c994f3ced1e82db0a1f6d46
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-02-23 20:45:19 +0100

    Update feur.py

--- feur/feur.py
+++ feur/feur.py
@@ -1,43 +1,35 @@
-class Deck(commands.Cog):
-    """Deck des catastrophes"""
+from redbot.core import commands
+import re
+
+list = ["Quoi", "quoi"]
+
+class Feur(commands.Cog):
+    """Quoi ? Feur"""
 
     def __init__(self, bot):
         self.bot = bot
-    @commands.group(name = "deck", invoque_without_command = True)
-    async def deckmain(self, ctx): #tire une carte tout le temps, commande à redef
+    @commands.group(name = "feur", invoque_without_command = True)
+    async def feurmain(self, ctx) :
         pass
-    
-    @deckmain.command(name = "draw")
-    async def drawmain(self, ctx):
-        """Tire une carte du deck des catastrophes"""
-        carte = random.choice(tuple(CarteDeck.keys()))
-        Rep = "Vous avez tiré la carte : " + str(carte)
-        await ctx.send(Rep)
-
-    @deckmain.command(name = "effet")
-    async def effetmain(self, ctx, Carte2):
-        """Donne l'effet d'une carte du deck. Le nom de la carte doit être entre guillemet."""
-        if Carte2 in CarteDeck : 
-            Rep = CarteDeck[Carte2]
-            Rep2 = str(Carte2)+ " : " + str(Rep)
-            await ctx.send(Rep2)
+    @feur.command(name = "activate")
+    async def actimain(self, ctx) :
+        activate = True
+        if activate == True : 
+            activate = False
+            await ctx.send("Feur désactivé")
         else :
-            await ctx.send("Le nom de la carte est invalide.")
+            activate = True
+            await ctx.send("Feur activé")
+     
+    @feur.event
+    async def on_message(message):
+        if activate == True :
+            for l in list :
+                if re.search(l, message.content) :
+                    await ctx.send("Feur")
+                else : 
+                    pass
+        else :
+            pass
     
-    @deckmain.command(name = "list") #Ne marche pas, surement à cause de la limite de message de discord
-    async def listmain(self, ctx):
-        """Liste l'intégralité des cartes du deck ainsi que leur effet."""
-        await ctx.send("Liste des effets du deck : \n") 
-        Rep3 = ""
-        for i, j in CarteDeck.items() :
-            Rep3 = Rep3 + str(i) + " : " + str(j) + "\n"
-            if len(Rep3) >= 1500 :
-                await ctx.send(Rep3)
-                Rep3 = ""
-        await ctx.send(Rep3)
-        
-    @deckmain.command(name = "nombre")
-    async def nombremain(self, ctx):
-       """Donne le nombre de carte dans le deck"""
-       n = int(len(CarteDeck))
-       await ctx.send(n)
+    
