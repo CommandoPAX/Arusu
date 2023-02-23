6e91a530bd8302d0044e586eadc099fd074ab20d
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-02-23 20:54:36 +0100

    Update feur.py

--- feur/feur.py
+++ feur/feur.py
@@ -4,10 +4,10 @@ import re
 from redbot.core.commands import Cog
 
 list = ["Quoi", "quoi"]
+activate = 1
 
 class Feur(commands.Cog):
     """Quoi ? Feur"""
-    activate = False
 
     def __init__(self, bot):
         self.bot = bot
@@ -16,16 +16,16 @@ class Feur(commands.Cog):
         pass
     @feurmain.command(name = "activate")
     async def actimain(self, ctx) :
-        if activate == True : 
-            activate = False
+        if activate == 1 : 
+            activate = 2
             await ctx.send("Feur désactivé")
         else :
-            activate = True
+            activate = 1
             await ctx.send("Feur activé")
      
     @Cog.listener()
     async def on_message(message : discord.Message):
-        if activate == True :
+        if activate == 1 :
             for l in list :
                 if re.search(l, message.content) :
                     await ctx.send("Feur")
