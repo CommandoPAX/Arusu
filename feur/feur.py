bca6cd986ae6bd3a08e7d44eb76dfde64e3b00f9
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-02-24 18:38:28 +0100

    Update feur.py

--- feur/feur.py
+++ feur/feur.py
@@ -2,23 +2,47 @@ from redbot.core import commands
 import discord
 import re
 from redbot.core.commands import Cog
+from typing import Union, Set, Literal, Optional
 
 list = ["Quoi", "quoi"]
 
 class Feur(commands.Cog):
     """Quoi ? Feur"""
     
-    def __init__(self, bot):
+    def __init__(self, bot: Red):
+        super().__init__()
         self.bot = bot
+        self.config = Config.get_conf(self, 4766951341)
+        default_guild_settings = {
+            "enabled" : False,
+        }
+        default_member_settings = {"filter_count": 0, "next_reset_time": 0}
+        default_channel_settings = {"filter": []}
+        self.config.register_guild(**default_guild_settings)
+        self.config.register_member(**default_member_settings)
+        self.config.register_channel(**default_channel_settings)
+        self.pattern_cache = {}
         
     @commands.group(name = "feur", invoque_without_command = True)
     async def feurmain(self, ctx) :
         pass
-     
-    @Cog.listener()
-    async def on_message(message):
-        for l in list :
-            if re.search(l, message.content) :
-                await ctx.send("Feur")
-            else : 
-                pass
+    
+    @feurmain.command()
+    async def enable(self, ctx):
+        """Active le plugin"""
+        await self.config.guild(ctx.guild).enabled.set(True)
+        await ctx.send(("Feur activé"))
+    
+    @feurmain.command()
+    async def disable(self, ctx):
+        """Désactive le plugin"""
+        await self.config.guild(ctx.guild).enabled.set(False)
+        await ctx.send(("Feur désactivé"))
+        
+    @commands.Cog.listener()
+    async def on_message(self, ctx, message: discord.Message):
+        if "quoi" in message.content.lower() :
+            await ctx.send("Feur")
+        else : 
+            pass
+        
