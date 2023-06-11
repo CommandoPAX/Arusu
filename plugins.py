bff4ca22a9a8a7e85462bb7706b3330be1e0621a
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-11 08:03:05 +0200

    Update plugins.py

--- plugins.py
+++ plugins.py
@@ -1,14 +1,38 @@
 import importlib
+from discord.ext import commands
+import discord
 plugins = {}
 
-class PluginManager():
-    
+class PluginManager(commands.Cog) :
+    def __init__(self, bot):
+        self.bot = bot
+        
+    def setup(bot) : 
+        bot.add_cog(PluginManager(bot))
+        
+    @commands.command()
+    async def load(self, ctx, TBL) :
+        try :
+            await self.bot.load(TBL) 
+        except :
+            await ctx.send("Unable to load cog")
+            
+    @commands.command()
+    async def unload(self, ctx, TBUL) : 
+        try :
+            await self.bot.unload(TBUL)
+        except :
+            await ctx.send("Unable to unload cog")
+            
+    '''@commands.command()
     async def importall():
         for cog in plugins.index() :
             importlib.import_module(name = plugins[cog])
-            
+        
+    @commands.command()  
     async def addcog(cogname) : 
         plugins[cogname] = "./plugins/" + cogname
         
+    @commands.command()
     async def rmcog(cogname) : 
-        del plugins[cogname]
+        del plugins[cogname]'''
