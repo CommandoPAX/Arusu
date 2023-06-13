ee73c3bb52501f965ee6f04d99d1d14e875c9336
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-13 16:11:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import os
 import discord
+import asyncio
 from discord.ext import commands
-from .plugins import *
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-#client = discord.Client()
+TOKEN = "TOKEN"
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+async def load() :
+    for filename in os.listdir("./plugins"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"plugins.{filename[:-3]}")
+
+async def main():
+    await load()
+    await bot.run(TOKEN)
+
+asyncio.run(main())
 
 @bot.event
-async def on_ready(self, bot):
-    await bot.load("PluginManager") #load PluginManager
-
-#client.run(TOKEN)
-bot.run(TOKEN)
+async def on_ready():
+    print("Hello world")
