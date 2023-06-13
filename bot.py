6029d47a6624ba020b5e050ced44e635bc13fe79
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-13 16:43:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
+#Core d'Arusu
+
 import os
 import discord
 import asyncio
 from discord.ext import commands
 
-TOKEN = "TOKEN"
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) #Apparament un bug connu sur Windows
 
-bot = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+TOKEN = "OTAzNjcwODM4NDM4NTU5ODA2.GaLZnB.2iZ-uhX41-PzDRyAMhA7-BsXVM_SJ6WET-LOlY"
+
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all()) #set le prefix et les intents
 
 async def load() :
-    for filename in os.listdir("./plugins"):
-        if filename.endswith(".py"):
-            await bot.load_extension(f"plugins.{filename[:-3]}")
+    for filename in os.listdir("./plugins"): #check le dossier plugins pour chaque fichier
+        if filename.endswith(".py"): #sinon c'est pas des modules
+            await bot.load_extension(f"plugins.{filename[:-3]}") #ajoute le module au bot
 
-async def main():
+async def main(): #fonction qui load les modules et démarre le bot
     await load()
-    await bot.run(TOKEN)
+    await bot.start(TOKEN) #démarre le bot
 
 asyncio.run(main())
-
-@bot.event
-async def on_ready():
-    print("Hello world")
