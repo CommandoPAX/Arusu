64d60bab817d2e8cb5b276bc011399f2af6b875f
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-19 04:26:38 +0200

    Add files via upload

---
+++ __path__/audio.py
@@ -0,0 +1,109 @@
+# Plugin gérant l'audio
+
+#ne marche pas
+
+import discord
+from discord.ext import commands
+import youtube_dl
+import asyncio
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+class audio(commands.Cog) :
+
+    def __init__(self, bot) :
+        self.bot = bot
+
+    @commands.command(name='join', description='Tells the bot to join the voice channel', usage = "!join")
+    async def join(ctx):
+        if not ctx.message.author.voice:
+            await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+            return
+        else:
+            channel = ctx.message.author.voice.channel
+        await channel.connect()
+
+    @commands.command(name='leave', description='To make the bot leave the voice channel', usage = "!leave")
+    async def leave(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_connected():
+            await voice_client.disconnect()
+        else:
+            await ctx.send("The bot is not connected to a voice channel.")
+
+    @commands.command(name='play', description='Plays the audio from a youtube video', usage = "!play [url]")
+    async def play(ctx,url):
+        try :
+            server = ctx.message.guild
+            voice_channel = server.voice_client
+
+            async with ctx.typing():
+                filename = await YTDLSource.from_url(url) #j'ai viré un loop = bot.loop
+                voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+            await ctx.send('**Now playing:** {}'.format(filename))
+        except:
+            await ctx.send("The bot is not connected to a voice channel.")
+
+
+    @commands.command(name='pause', usage = "!pause", description = "Pause the music")
+    async def pause(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.pause()
+        else:
+            await ctx.send("The bot is not playing anything at the moment.")
+        
+    @commands.command(name='resume', usgae = "!resume", description='Resumes the music')
+    async def resume(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_paused():
+            await voice_client.resume()
+        else:
+            await ctx.send("The bot was not playing anything before this. Use play_song command")
+
+    @commands.command(name='stop', description='Stops the music', usage = "!stop")
+    async def stop(ctx):
+        voice_client = ctx.message.guild.voice_client
+        if voice_client.is_playing():
+            await voice_client.stop()
+        else:
+            await ctx.send("The bot is not playing anything at the moment.")
+
+async def setup(bot : commands.Bot) :
+    await bot.add_cog(audio(bot))
