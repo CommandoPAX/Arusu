8fd15b708d655116e68ed5ef73fffa505d6b1efd
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-19 04:27:14 +0200

    Update utils.py

--- __path__/utils.py
+++ __path__/utils.py
@@ -8,14 +8,20 @@ class utils(commands.Cog) :
     def __init__(self, bot) :
         self.bot = bot
 
-    @commands.command(name="status", usage = "<usage>", description = "Change le statut du bot")
+    @commands.command(name="status", usage = "<usage>", description = "Change le statut du bot", help = "changes the bot's status")
     @commands.is_owner()
     async def status(self, ctx):
+        """
+        change le statut, à réimplémenter d'une manière qui marche
+        """
         #await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Streaming("le code source d'Arusu"))
-        await ctx.send("Arusu Dev is Online")
+        await ctx.send("Statut changé")
 
-    @commands.command(name = "ping", usage = "<usage>", description = "description")
+    @commands.command(name = "ping", usage = "<usage>", description = "description", help = "Check to see if the bot is lagging")
     async def ping(self, ctx:commands.Context):
+        """
+        Ping le bot
+        """
         await ctx.send("Pong !")
 
 async def setup(bot : commands.Bot) :
