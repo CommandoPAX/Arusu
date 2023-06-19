cea2254497c57d62038d034567eb017fedbec39d
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-19 18:04:30 +0200

    Update utils.py

--- __path__/utils.py
+++ __path__/utils.py
@@ -2,27 +2,39 @@
 
 import discord
 from discord.ext import commands
+import sys
+import os
 
 class utils(commands.Cog) :
 
     def __init__(self, bot) :
         self.bot = bot
-
-    @commands.command(name="status", usage = "<usage>", description = "Change le statut du bot", help = "changes the bot's status")
-    @commands.is_owner()
-    async def status(self, ctx):
-        """
-        change le statut, à réimplémenter d'une manière qui marche
-        """
-        #await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Streaming("le code source d'Arusu"))
-        await ctx.send("Statut changé")
-
-    @commands.command(name = "ping", usage = "<usage>", description = "description", help = "Check to see if the bot is lagging")
+    
+    @commands.command(name = "ping", usage = "!ping", description = "Pings the bot")
     async def ping(self, ctx:commands.Context):
         """
-        Ping le bot
+        Pings the bot
         """
         await ctx.send("Pong !")
+
+    @commands.command(name = "restart", usage = "!restart", description = "Restarts the bot")
+    @commands.is_owner()
+    async def restart(self, ctx) :
+        """
+        Restarts the bot
+        """
+        await ctx.send("Restarting bot...")
+        os.execv(sys.executable, ['python'] + sys.argv) #the part that restarts the bot
+
+    @commands.command(name = "shutdown", usage = "!shutdown", description = "Shutdown the bot")
+    @commands.is_owner()
+    async def shutdown(self, ctx) :
+        """
+        shutdown the bot
+        """
+        await ctx.send("Shutting down...")
+        await self.bot.close()
+        print("Bot is offline")
 
 async def setup(bot : commands.Bot) :
     await bot.add_cog(utils(bot))
