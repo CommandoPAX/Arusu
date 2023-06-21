3a3f3e2b300f8ac467152aca0b32eb799a28790f
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-21 03:16:02 +0200

    Update utils.py

--- __path__/utils.py
+++ __path__/utils.py
@@ -10,14 +10,14 @@ class utils(commands.Cog) :
     def __init__(self, bot) :
         self.bot = bot
     
-    @commands.command(name = "ping", usage = "!ping", description = "Pings the bot")
+    @commands.command(name = "ping", usage = "", description = "Pings the bot")
     async def ping(self, ctx:commands.Context):
         """
         Pings the bot
         """
         await ctx.send("Pong !")
 
-    @commands.command(name = "restart", usage = "!restart", description = "Restarts the bot")
+    @commands.command(name = "restart", usage = "", description = "Restarts the bot")
     @commands.is_owner()
     async def restart(self, ctx) :
         """
@@ -26,7 +26,7 @@ class utils(commands.Cog) :
         await ctx.send("Restarting bot...")
         os.execv(sys.executable, ['python'] + sys.argv) #the part that restarts the bot
 
-    @commands.command(name = "shutdown", usage = "!shutdown", description = "Shutdown the bot")
+    @commands.command(name = "shutdown", usage = "", description = "Shutdown the bot")
     @commands.is_owner()
     async def shutdown(self, ctx) :
         """
@@ -35,6 +35,13 @@ class utils(commands.Cog) :
         await ctx.send("Shutting down...")
         await self.bot.close()
         print("Bot is offline")
+
+
+    ###################################################################################################################################
+
+    @commands.Cog.listener(name = "on_ready")
+    async def ConfirmStart(self) :
+        print("Bot online")
 
 async def setup(bot : commands.Bot) :
     await bot.add_cog(utils(bot))
