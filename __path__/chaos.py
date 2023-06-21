fb58ac7176d8104e75a483dc0be397f8a7e1f344
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-06-21 18:38:42 +0200

    Update chaos.py

--- __path__/chaos.py
+++ __path__/chaos.py
@@ -44,34 +44,31 @@ class Chaos(commands.Cog):
     @commands.is_owner()
     async def add(self, ctx, role : discord.Role):
         """Ajoute un role"""
-        async with self.roles as roles:
-            if role in roles:
-                await ctx.send("Le role est déjà dans la liste")
-                return
-            roles.append(role)
-            await ctx.send(("{} ajouté à la liste").format(role.mention))
+        if role in self.roles:
+            await ctx.send("Le role est déjà dans la liste")
+            return
+        self.roles.append(role)
+        await ctx.send(("{} ajouté à la liste").format(role.mention))
 
     @chaos.command()
     @commands.is_owner()
     async def remove(self, ctx, role: discord.Role):
         """Enleve un role"""
-        async with self.roles as roles:
-            if role not in roles:
-                await ctx.send("Le role n'est pas dans la liste")
-                return
-            roles.remove(role)
-            await ctx.send(("{} enlevé de la liste").format(role.mention))
+        if role not in self.roles:
+            await ctx.send("Le role n'est pas dans la liste")
+            return
+        self.roles.remove(role)
+        await ctx.send(("{} enlevé de la liste").format(role.mention))
     
     @chaos.command()
     @commands.is_owner()
     async def list(self, ctx):
         """Liste tout les roles"""
-        async with self.roles as roles:
-            if not roles:
-                await ctx.send("Aucun role dans la liste")
-                return
-            role_mentions = [ctx.guild.get_role(role_id).mention for role_id in roles]
-            await ctx.send(("Liste: {}").format(", ".join(role_mentions)))
+        if not self.roles:
+            await ctx.send("Aucun role dans la liste")
+            return
+        role_mentions = [role_id.mention for role_id in self.roles]
+        await ctx.send(("Liste: {}").format(", ".join(role_mentions)))
 
     @chaos.command()
     async def activate(self, ctx):
@@ -82,9 +79,9 @@ class Chaos(commands.Cog):
             member = ctx.message.author
             if self.chaoswitch != True :
                 return
-            await member.add_roles(*[member.guild.get_role(role_id) for role_id in self.roles])
+            await member.add_roles(*[role_id for role_id in self.roles])
             await asyncio.sleep(120)
-            await member.remove_roles(*[member.guild.get_role(role_id) for role_id in self.roles])
+            await member.remove_roles(*[role_id for role_id in self.roles])
         except Exception as e :
             await ctx.send("Le chaos n'a pas pu être activé.")
             print(e)
