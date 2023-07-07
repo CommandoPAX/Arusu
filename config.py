7d230696ca3145966c08bdc3f7c077853ae67985
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-07 17:38:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,15 @@ import os
 
 class ArusuConfig() : 
     def __init__(self) :
-        self.BOT_PREFIX = '?'
-        with open(r"C:\Users\bruan\Pictures\Bot Discord\ArusuDev\config.json", 'r') as f:
+        with open(r"..\config.json", 'r') as f:
             self.DATA = json.load(f)
-                
+    
+    def update(self, index, value) : 
+        try :
+            self.DATA[index] = value
+            with open(r"..\config.json", 'w') as outf :
+                json.dump(self.DATA, outf)
+            print(index, "has been updated to", value)
+        except Exception as e:
+            print("Value could not be updated") 
+            print(e)
