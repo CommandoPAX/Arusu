a972c477914e1a837d61442764d66f1875ee4bb1
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-07 17:40:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,15 +6,16 @@ import os
 
 class ArusuConfig() : 
     def __init__(self) :
-        with open(r"..\config.json", 'r') as f:
+        with open(r".\config.json", 'r') as f:
             self.DATA = json.load(f)
     
     def update(self, index, value) : 
         try :
             self.DATA[index] = value
-            with open(r"..\config.json", 'w') as outf :
+            with open(r".\config.json", 'w') as outf :
                 json.dump(self.DATA, outf)
             print(index, "has been updated to", value)
         except Exception as e:
             print("Value could not be updated") 
             print(e)
+                
