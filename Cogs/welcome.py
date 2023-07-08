527d4ca4474efc5e379335f97dcc1a3d13f51b96
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-08 03:15:12 +0200

    Update welcome.py

--- Cogs/welcome.py
+++ Cogs/welcome.py
@@ -2,73 +2,87 @@
 
 import discord
 from discord.ext import commands
+from config import ArusuConfig
 
 class welcome(commands.Cog) :
 
     def __init__(self, bot) :
         self.bot = bot
-        self.wchannel = 0
-        self.lchannel = 0
-        self.WelcomeMSG = f"Bienvenue dans le serveur {discord.Member}"
-        self.LeaveMSG = f"{discord.Member} a quitté le serveur."
+        self.config = ArusuConfig()
     
     @commands.command(name = "welcomeset", usage = "[channel ID]", description = "Sets the welcome channel")
     @commands.is_owner()
-    async def welcomeset(self, ctx, channelID) :
+    async def welcomeset(self, ctx) :
         """
-        Sets the welcome message channel, 0 to disable
+        Sets the welcome message channel, currently only works on S;G France
         """
-        self.wchannel = self.bot.get_channel(channelID)
-        await ctx.send(f"Welcome channel set to {self.wchannel}")
+        try :
+            self.config.update(f"{ctx.guild.id}.welcome_channel", self.bot.get_channel(482256698342506508).id)
+            await ctx.send(f"Welcome channel set to {self.config.DATA[f'{ctx.guild.id}.welcome_channel']}")
+        except Exception as e :
+            await ctx.send("Could not set welcome channel")
+            print(e)
 
     @commands.command(name = "welcomemsg", usage = "[your welcome message]", description = "Sets the welcome message")
     @commands.is_owner()
-    async def welcomemsg(self, ctx, message) :
+    async def welcomemsg(self, ctx, welcomemsg) :
         """
         Sets the welcome message
         """
-        self.WelcomeMSG = message
-        await ctx.send("Welcome message set to : ", self.WelcomeMSG)
+        try : 
+            self.config.update(f"{ctx.guild.id}.welcome_message", welcomemsg)
+            member = ctx.author
+            await ctx.send("Welcome message set to : ")
+            await ctx.send((self.config.DATA[f"{ctx.guild.id}.welcome_message"]).format(Member = member.mention, Server = ctx.guild))
+        except Exception as e :
+            await ctx.send("Could not set welcome message")
+            print(e)
 
     @commands.command(name = "leaveset", usage = "[channel ID]", description = "Sets the leave channel")
     @commands.is_owner()
-    async def leaveset(self, ctx, channelID) :
+    async def leaveset(self, ctx) :
         """
-        Sets the leave message channel, 0 to disable
+        Sets the leave message channel, currently only works on S;G France
         """
-        self.lchannel = self.bot.get_channel(channelID)
-        await ctx.send(f"Welcome channel set to {self.lchannel}")
+        try :
+            self.config.update(f"{ctx.guild.id}.leave_channel", self.bot.get_channel(721312376225398785).id)
+            await ctx.send(f"Leave channel set to {self.config.DATA[f'{ctx.guild.id}.leave_channel']}")
+        except Exception as e :
+            await ctx.send("Could not set leave channel")
+            print(e)
 
     @commands.command(name = "leavemsg", usage = "[message]", description = "Sets the leave message")
     @commands.is_owner()
-    async def leaveemsg(self, ctx, message) :
+    async def leaveemsg(self, ctx, leavemsg) :
         """
         Sets the leave message
         """
-        self.LeaveMSG = message
-        await ctx.send("Welcome message set to : ", self.LeaveMSG)
+        try : 
+            self.config.update(f"{ctx.guild.id}.leave_message", leavemsg)
+            member = ctx.author
+            await ctx.send("Leave message set to : ")
+            await ctx.send((self.config.DATA[f"{ctx.guild.id}.leave_message"]).format(Member = member.mention, Server = ctx.guild))
+        except Exception as e :
+            await ctx.send("Could not set leave message")
+            print(e)
 
     ###################################################################################################################################
 
     @commands.Cog.listener(name = "on_member_join")
-    async def welcome(self, member) :
+    async def welcome(self, ctx, member) :
         try : 
-            if self.wchannel != 0 :
-                await self.wchannel.send(self.WelcomeMSG)
-            else : 
-                pass
-        except Exception as e :
-            print(e)
+            channel = self.bot.get_channel(self.config.DATA[f'{ctx.guild.id}.welcome_channel'])
+            await channel.send((self.config.DATA[f"{ctx.guild.id}.welcome_message"]).format(Member = discord.Member, serveur = ctx.guild))
+        except :
+            print("Could not send welcome message")
     
     @commands.Cog.listener(name = "on_member_remove")
-    async def leave(self, member) :
+    async def leave(self, ctx, member) :
         try : 
-            if self.lchannel != 0 :
-                await self.lchannel.send(self.LeaveMSG)
-            else : 
-                pass
-        except Exception as e :
-            print(e)
+            channel = self.bot.get_channel(self.config.DATA[f'{ctx.guild.id}.leave_channel'])
+            await channel.send((self.config.DATA[f"{ctx.guild.id}.leave_message"]).format(Member = discord.Member, serveur = ctx.guild))
+        except :
+            print("Could not send leave message")
 
 async def setup(bot : commands.Bot) :
     await bot.add_cog(welcome(bot))
