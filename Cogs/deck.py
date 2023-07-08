b40a8a1aefcff2f305bad8dfb20529af0c7c6d6b
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-08 03:13:52 +0200

    Update deck.py

--- Cogs/deck.py
+++ Cogs/deck.py
@@ -2,46 +2,45 @@
 
 from discord.ext import commands
 import random
+import json
 
-CarteDeck = {"Balance" : "Inverse votre alignement", "Comète" : "Oblitère un ennemi aléatoire durant le prochain combat", "Donjon" : "Vous êtes téléportez dans un studio parisien enfoui 10m sous votre ancienne position",
-            "Général Puzzle" : "-1 à vos jets de save pour toujours", "Destin" : "Vous pouvez revenir dans le temps pour annuler un évenement", "Feu" : "Un ennemi puissant vous traque",
-            "Idiot" : "-1 a tout vos attributs et vous retirez une carte", "Voyage astral à sans unique" : "Votre âme est envoyer dans le plan astral, aucun retour possible sans aide extérieur. Dans le plan astral, cette carte pointe en permanence vers la direction de votre âme",
-            "Soldat" : "Vous gagnez un lance-fusée capable d'invoquer vos alliés peu importe leur position", "Lune" : "Vous pouvez utiliser 1d4 wish",
-            "Ruine" : "Vos 3 prochains jets de save ratent à coup sûr", "Etoile" : "Votre corps se met à émettre de la lumière",
-            "Soleil" : "Full HP, et vous gagnez une arme de votre choix", "Terre" : "Tout vos objets perdent leur propriété spéciale",
-            "Trône" : "+30 à votre prochain jet de diplomatie", "Vizier" : "Vous connaitrez la prochaine réponse à une unique question",
-            "Vide" : "Vous êtes transporté dans une dimension/plan aléatoire. Le nom du plan/dimension est inscrit sur la carte qui reste à votre emplacement d'origine", 
-            "Animal" : "Vous devenez un animal aléatoire",
-            "Climatosceptique" : "Vous avez peur des arbres", "Nat 1" : "Vous tirez deux autres cartes", "Nat 20" : "Vous forcez quelqu'un à tirer deux cartes",
-            "Gemme" : "Vous gagnez 100 000 golds", "Joker" : "+1 à tout vos attributs", "Voyage" : "Vous pouvez cast Téléportation une fois en utilisant cette carte",
-            "Ombre" : "Fait spawn un double de vous-même que vous devez affronter et éliminer seul, si quelqu'un vous aide, une ombre de cette personne apparait",
-            "Magicien" : "Vous accorde 4 sorts de niveau 5 ou - que vous choisissez, vous obtenez un spell slot par sort du même niveau que vous avez choisi.",
-            "Roue" : "Vous ne pouvez plus vous déplacer qu'en faisant des saltos", "Loto" : "Vous pouvez reroll vos 3 prochains lancer de dés",
-            "Expérience" : "Vous montez de niveau", "Hiérophante" : "Vous obtenez le livre sacrée de la religion vous convenant le plus",
-            "Les Amoureux" : "Vous êtes liés à la personne la plus proche de vous. Vous ne pouvez plus vous éloignez à plus de 10m ou vous subissez 1d12 dégâts purs par tour.",
-            "Le Monde" : "Tout le monde dans un rayon de 20m du tireur original tire une carte, y compris le tireur original",
-            "Temps" : "Vous ne pouvez plus vieillir", "L'equilibre" : "Vos échecs et réussites critiques sont inversés.", "Ambivalent" : "Vous devez multiclass au niveau suivant",
-            "Paladin" : "Choisissez un autre joueur, tout dégâts qu'il prend le passant à moins de 1PV vous sont transféré.",
-            "Faiblesse" : "Les 5 prochaines attaques que vous subissez vous infligent 2 fois plus de dégâts",
-            "Traitrise" : "Tirez des cartes jusqu'à en piocher 3 négatives. Vous infligez ces effets à un ou plusieurs alliés de votre choix",
-            "Doppelganger" : "Vous gagnez un passif aléatoire d'une classe aléatoire même si celui-ci est inutile",
-            "Baleine orbitale" : "Une baleine vous tombe dessus, vous infligeant 6d6 dégâts purs et aux créatures dans un rayon de moins de 6m",
-            "Pigeon" : "Vous perdez toutes vos richesses", "Le soigneur" : "Vous gagnez 1d10 PV max", "Empereur" : "Vous pouvez cast domination une fois par jour",
-            "Le politicien" : "Vous détectez tout les mensonges prononcé en votre présence", "La force" : "Vous réussisez tout les jets impliquant la force pendant 24h",
-            "Hermite" : "Vous tombez inconscient", "Pendu" : "Vous perdez la moitié de vos PV", "Tempérance" : "Votre alignement devient Neutre Neutre",
-            "Cours d'algèbre surprise" : "+2 à votre attribut d'intelligence mais vous devenez confus pendant 1d3 jours", "Aléatoire" : "Tout les effets des cartes que vous tirez sont transféré à une personne proche aléatoire",
-            "Pentacle" : "Vous perdez tout votre équipement", "Dague" : "Vous perdez 1d4 PV", "Oubli" : "Tout le monde oublie votre existence jusqu'à l'instant présent",
-            "Vision" : "Vous pouvez dorénavant voir à travers toutes les surfaces et illusions", "Thanos" : "Un de vos alliés et un de vos ennemis meurt de facon permanente. Vous devez choisir qui meurt, vous pouvez vous choisir", "Conquête spatiale" : "Vous téléporte sur une planète aléatoire",
-            "Trou noir" : "Invoque un trou noir sur votre position", "Ecureuil" : "Transforme d10 personnes à moins de 20m en écureuils pendant 1d3 heures",
-            "Jouvence" : "Vous rajeunissez de 5d10+10 années pour un âge minimum de 3ans", "Stop" : "Vous ne pouvez plus entreprendre aucune action pendant 1d10 minutes",
-            "Lag" : "Vous perdez vos deux prochains tours", "Reconversion professionnelle" : "Tout vos niveaux actuels sont transférés vers une autre classe de votre choix",
-            "Cryptophonie quantique" : "vos paroles ne peuvent plus être comprise que par une personne a la fois", "Farine" : "Votre corps se transforme en farine, vous devenez étherée mais obtenez une vulnérabilité au feu (x2)",
-            "Quoi ? Feur" : "A chaque fois que quelqu'un finit une phrase par quoi, vous devez répondre feur ou subir 1d6 points de dégâts purs", "Ambidextre involontaire" : "Vos deux mains sont inversés, vous subissez un malus de -4 à votre dextérité"}
+class DeckCatastrophe() :
+    def __init__(self) :
+        with open(r"Cogs\DeckCatastrophe.json", 'r') as f:
+            self.CARDS = json.load(f)
+    
+    def add_card(self, NAME, Effect) :
+        try :
+            self.CARDS[NAME] = Effect
+            with open(r"Cogs\DeckCatastrophe.json", 'w') as outf :
+                    json.dump(self.CARDS, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
+            
+        except :
+            print("Could not add card")
 
-'''Carte retirée : 
-"Mort" : "Vous mourrez"
-"Baleine (ancienne version)" : "Effet similaire à la carte mort mais c'est une baleine donc c'est pire",
-'''
+    def rm_card(self, NAME) :
+        try :
+            del self.CARDS[NAME]
+            with open(r"Cogs\DeckCatastrophe.json", 'w') as outf :
+                    json.dump(self.CARDS, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
+        except :
+            print("Could not remove card")
+
+    def list(self) : #J'ai pas de solution très élégante pour le moment en considérant la limite de charactère de discord
+        pass
+    
+    def count(self) :
+        return int(len(self.CARDS))
+
+    def effect(self, Carte) :
+        try :
+            if Carte in self.CARDS : 
+                CardEffect = str(Carte)+ " : " + str(self.CARDS[Carte])
+                return CardEffect
+            else :
+                return "Invalid card name"
+        except :
+            return "Could not send card effect"
 
 class Deck(commands.Cog):
     """
@@ -50,49 +49,67 @@ class Deck(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.Deck = DeckCatastrophe()
         
-    @commands.group(name = "deck", invoque_without_command = True, aliases = ["dick"])
-    async def deckmain(self, ctx): #Utilise le framework de base pour list les arguments
-        """
-        Commande maitre pour gérer le deck des catastrophes
-        """
-        pass
-    
-    @deckmain.command(name = "draw", usage = "[nombre]", description = "Tire une carte du deck des catastrophes")
+    @commands.command(name = "deck", usage = "[nombre]", description = "Tire une carte du deck des catastrophes", aliases = ["dick"])
     async def drawmain(self, ctx, nb = 1):
         """Tire une carte du deck des catastrophes"""
         for i in range (0, int(nb)):
-              carte = random.choice(tuple(CarteDeck.keys()))
-              Rep = "Vous avez tiré la carte : " + str(carte) + "\n" + str(CarteDeck[carte])
+              carte = random.choice(tuple(self.Deck.CARDS.keys()))
+              Rep = "Vous avez tiré la carte : " + str(carte) + "\n" + str(self.Deck.CARDS[carte])
               await ctx.send(Rep)
-
-    @deckmain.command(name = "effet", usage = '["Carte"]', description = "Donne l'effet d'une carte du deck. Le nom de la carte doit être entre guillemet.")
-    async def effetmain(self, ctx, Carte2):
-        """Donne l'effet d'une carte du deck. Le nom de la carte doit être entre guillemet."""
-        if Carte2 in CarteDeck : 
-            Rep = CarteDeck[Carte2]
-            Rep2 = str(Carte2)+ " : " + str(Rep)
-            await ctx.send(Rep2)
-        else :
-            await ctx.send("Le nom de la carte est invalide.")
     
-    @deckmain.command(name = "list", usage = "", description = "Liste l'intégralité des cartes du deck ainsi que leur effet.")
+    @commands.command(name = "deck_effect", usage = '["Carte"]', description = "Donne l'effet d'une carte du deck. Le nom de la carte doit être entre guillemet.")
+    async def effetmain(self, ctx, CardName):
+        """Donne l'effet d'une carte du deck. Le nom de la carte doit être entre guillemet."""
+        await ctx.send(self.Deck.effect(CardName))
+    
+    @commands.command(name = "deck_list", usage = "", description = "Liste l'intégralité des cartes du deck ainsi que leur effet.")
     async def listmain(self, ctx):
         """Liste l'intégralité des cartes du deck ainsi que leur effet."""
         await ctx.send("Liste des effets du deck : \n") 
         Rep3 = ""
-        for i, j in CarteDeck.items() :
+        for i, j in self.Deck.CARDS.items() :
             Rep3 = Rep3 + str(i) + " : " + str(j) + "\n"
             if len(Rep3) >= 1500 :
                 await ctx.send(Rep3)
                 Rep3 = ""
         await ctx.send(Rep3)
         
-    @deckmain.command(name = "nombre", usage = "", description = "Donne le nombre de carte dans le deck")
+    @commands.command(name = "deck_count", usage = "", description = "Donne le nombre de carte dans le deck")
     async def nombremain(self, ctx):
        """Donne le nombre de carte dans le deck"""
-       n = int(len(CarteDeck))
-       await ctx.send(n)
+       await ctx.send(self.Deck.count())
+
+    @commands.group(name = "deck_set", invoque_without_command = True, aliases = ["dick_set"])
+    @commands.is_owner()
+    async def deckmain(self, ctx): #Utilise le framework de base pour list les arguments
+        """
+        Commande maitre pour gérer le deck des catastrophes
+        """
+        pass    
+
+    @deckmain.command(name = "add", usage = '["Card Name"] ["Card Effect]', description = "Adds a card to the deck")
+    async def add_cmd(self, ctx, CardName, CardEffect) :
+        """
+        Adds a card to the deck
+        """
+        try :
+            self.Deck.add_card(NAME= CardName, Effect=CardEffect)
+            await ctx.send(f"`{CardName} : {CardEffect}` has been added to the Deck.")
+        except :
+            await ctx.send("Could not add card to deck")
+
+    @deckmain.command(name ="remove", usage = '["Card Name]', description = "Removes a card from the deck")
+    async def rm_cmd(self, ctx, CardName) :
+        """
+        Removes a card from the deck
+        """
+        try :
+            self.Deck.rm_card(NAME=CardName)
+            await ctx.send(f"`{CardName}` has been removed from the deck.")
+        except :
+            await ctx.send(f"Could not remove `{CardName}` from the deck.")
 
 async def setup(bot : commands.Bot) :
     await bot.add_cog(Deck(bot))
