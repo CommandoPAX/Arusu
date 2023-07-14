7ef9d9de3899a1193d1308a7f11e26bbe136ba51
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-14 11:16:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 # Plugin gérant la config d'Arusu
 
 import json
+import platform
 
 class ArusuConfig() : 
     def __init__(self) :
-        with open(r".\config.json", 'r', encoding='utf-8') as f:
-            self.DATA = json.load(f)
+        if platform.system() == "Windows" :
+            with open(r".\config.json", 'r', encoding='utf-8') as f:
+                self.DATA = json.load(f)
+        if platform.system() == "Linux" :
+            with open(r"./config.json", 'r', encoding='utf-8') as f:
+                self.DATA = json.load(f)
     
     def update(self, index, value) : 
         try :
             self.DATA[index] = value
-            with open(r".\config.json", 'w', encoding='utf-8') as outf :
-                json.dump(self.DATA, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
+            if platform.system == "Linux" :
+                with open(r"./config.json", 'w', encoding='utf-8') as outf :
+                    json.dump(self.DATA, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
+            if platform.system == "Windows" :
+                with open(r".\config.json", 'w', encoding='utf-8') as outf :
+                    json.dump(self.DATA, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
             print(index, "has been updated to", value)
         except Exception as e:
             print("Value could not be updated") 
