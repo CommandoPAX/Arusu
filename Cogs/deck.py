f9c6e30f439a5e98d628a03db0e20189afc80c7b
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-14 19:01:37 +0200

    Update deck.py

--- Cogs/deck.py
+++ Cogs/deck.py
@@ -11,12 +11,16 @@ import platform
 
 class DeckCatastrophe() :
     def __init__(self) :
-        if platform.system() == "Linux" :
-            with open("Cogs/DeckCatastrophe.json", 'r', encoding='utf-8') as f:
-                self.CARDS = json.load(f)
-        if platform.system() == "Windows" :
-            with open(r"Cogs\DeckCatastrophe.json", 'r', encoding='utf-8') as f:
-                self.CARDS = json.load(f)
+        try :
+            if platform.system() == "Linux" :
+                with open("Cogs/DeckCatastrophe.json", 'r', encoding='utf-8') as f:
+                    self.CARDS = json.load(f)
+            if platform.system() == "Windows" :
+                with open(r"Cogs\DeckCatastrophe.json", 'r', encoding='utf-8') as f:
+                    self.CARDS = json.load(f)
+        except Exception as e :
+            print("Failed to initialize the deck")
+            print(e)
         self.config = ArusuConfig()
         self.items_per_page = 20
         self.pages = math.ceil(self.count() / self.items_per_page)
