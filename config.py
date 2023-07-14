36e92c5ed74d503f1a6f04d506d1029a6d2bd90b
Commando.PAX <34586377+CommandoPAX@users.noreply.github.com>
2023-07-14 14:18:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ class ArusuConfig() :
             with open(r".\config.json", 'r', encoding='utf-8') as f:
                 self.DATA = json.load(f)
         if platform.system() == "Linux" :
-            with open(r"./config.json", 'r', encoding='utf-8') as f:
+            with open("./config.json", 'r', encoding='utf-8') as f:
                 self.DATA = json.load(f)
     
     def update(self, index, value) : 
         try :
             self.DATA[index] = value
             if platform.system == "Linux" :
-                with open(r"./config.json", 'w', encoding='utf-8') as outf :
+                with open("./config.json", 'w', encoding='utf-8') as outf :
                     json.dump(self.DATA, outf, indent=4, separators=(", ", ": "), sort_keys=True, skipkeys=True, ensure_ascii=False)
             if platform.system == "Windows" :
                 with open(r".\config.json", 'w', encoding='utf-8') as outf :
