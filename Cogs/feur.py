c04553c772bd2426a6659bdfdd3a16ee71f46db9
CommandoPAX <bruant.theo@gmail.com>
2023-07-20 22:49:31 +0200

    Added the ability to enable/disable the feur answer

--- Cogs/feur.py
+++ Cogs/feur.py
@@ -10,44 +10,53 @@ class Feur(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = ArusuConfig()
-        self.feuractive = True
         
-    @commands.group(name = "feur", invoque_without_command = True)
+    @commands.group(name = "feur")
+    @commands.is_owner()
     async def feurmain(self, ctx) :
         pass
     
     @feurmain.command(name = "enable", usage = "", description = "Active le cog")
     async def enable(self, ctx):
         """Active le cog"""
-        if self.feuractive == True :
-            await ctx.send("Feur était déjà activé.")
-        else : 
-            self.feuractive = True
-            await ctx.send(("Feur activé"))
+        try :
+            if self.config.DATA[f"{ctx.guild.id}.FeurEnabled"] == 1:
+                await ctx.send("Feur était déjà activé.")
+            else : 
+                self.config.update(f"{ctx.guild.id}.FeurEnabled", 1)
+                await ctx.send(("Feur activé"))
+        except Exception as e:
+            print(e)
     
     @feurmain.command(name = "disable", usage = "", description = "Désactive le plugin")
     async def disable(self, ctx):
         """Désactive le plugin"""
-        if self.feuractive == False :
-            await ctx.send("Feur était déjà desactivé.")
-        else : 
-            self.feuractive = False
-            await ctx.send(("Feur désactivé"))
+        try :
+            if self.config.DATA[f"{ctx.guild.id}.FeurEnabled"] == 0 :
+                await ctx.send("Feur était déjà desactivé.")
+            else : 
+                self.config.update(f"{ctx.guild.id}.FeurEnabled", 0)
+                await ctx.send(("Feur désactivé"))
+        except Exception as e:
+            print(e)
 
     ################################################################################################################################### 
     
     @commands.Cog.listener(name = "on_message")
     async def FeurAnswer(self, message):
-        if message.author.id == self.bot.user.id:  #Stopping the bot from reading its own message
-            return
-        if self.feuractive == False : 
-            return
-        #Check if "quoi" is written
-        if re.search(r"\bquoi\b\W*$",message.content, flags=re.I) :
-            #Cases for "quoi" and "pourquoi" and "pour quoi"
-            await message.channel.send("Feur")
-        if re.search(r"\bpour ?quoi\b\W*$",message.content, flags=re.I) :
-            await message.channel.send("Pour feur")
+        try :
+            if message.author.id == self.bot.user.id:  #Stopping the bot from reading its own message
+                return
+            if self.config.DATA[f"{message.guild.id}.FeurEnabled"] != 1 : 
+                return
+            #Check if "quoi" is written
+            if re.search(r"\bquoi\b\W*$",message.content, flags=re.I) :
+                #Cases for "quoi" and "pourquoi" and "pour quoi"
+                await message.channel.send("Feur")
+            if re.search(r"\bpour ?quoi\b\W*$",message.content, flags=re.I) :
+                await message.channel.send("Pour feur")
+        except :
+            pass
             
 async def setup(bot : commands.Bot) :
     await bot.add_cog(Feur(bot))
