29fe793d40bca32487e7d8e9a34bf8b92e72758b
CommandoPAX <bruant.theo@gmail.com>
2023-07-21 18:47:44 +0200

    Modified utils

--- Cogs/utils.py
+++ Cogs/utils.py
@@ -13,70 +13,13 @@ class utils(commands.Cog) :
         self.bot = bot
         self.config = ArusuConfig()
         self.CogName = "utils"
-
-    @commands.command(name = "config_test", usage = "", description = "Tests the config plugin")
-    @commands.is_owner()
-    async def conftest(self, ctx) :
-        """
-        Tests Arusu's config
-        """
-        try :
-            await ctx.send(self.config.DATA[f"{ctx.guild.id}.Base"])
-        except :
-            self.config.update(f"{ctx.guild.id}.Base", "Arusu's config working")
-            await ctx.send(self.config.DATA[f"{ctx.guild.id}.Base"])
-
+        
     @commands.command(name = "ping", usage = "", description = "Pings the bot")
     async def ping(self, ctx:commands.Context):
         """
         Pings the bot
         """
         await ctx.send("Pong !")
-
-    @commands.command(name = "embed_colour", usage = "", description = "Changes the embed default color")
-    @commands.is_owner()
-    async def embed_colour_set(self, ctx, colour = "#1900ff") :
-        self.config.update("BOT_EMBED_COLOUR", colour)
-        try :
-            embed = discord.Embed(title="Default Embed", description=f"Colour changed to {colour}", color=discord.Color.from_str(colour))
-            await ctx.send(embed = embed)
-        except : 
-            await ctx.send("Invalid colour")
-
-    @commands.command(name = "showsettings", usage = "", description = "Shows Arusu's config")
-    @commands.is_owner()
-    async def showset(self, ctx) : 
-        """
-        Show Arusu's config
-        """
-        try :
-            Settings = ""
-            try :
-                for i in self.config.DATA.keys() :
-                    if i != "BOT_TOKEN" :
-                        Settings += f"**{i}** : {self.config.DATA[i]} \n"
-            except Exception as e :
-                print("Error : error in Settings generation")
-                print(e)
-
-            embed = (discord.Embed(title="Settings",
-                                description=Settings,
-                                color = discord.Color.from_str(self.config.DATA["BOT_EMBED_COLOUR"])))
-        except Exception as e :
-            print("Error : error in embed settings generation")
-            print(e)
-        await ctx.send(embed = embed)
-
-    @commands.command(name = "prefix", usage = "[prefix]", description = "Changes Arusu's prefix, requires a restart to take effect")
-    @commands.is_owner()
-    async def prefixset(self, ctx, NEWPREFIX) :
-        try :
-            self.config.update("BOT_PREFIX", str(NEWPREFIX))
-            await ctx.send(f"Prefix changed to {NEWPREFIX}")
-        except Exception as e :
-            await ctx.send("Could not change prefix")
-            print("Error in changing prefix")
-            print(e)
 
     @commands.command(name = "restart", usage = "", description = "Restarts the bot", aliases = ["r"])
     @commands.is_owner()
@@ -101,9 +44,66 @@ class utils(commands.Cog) :
         except Exception as e :
             await ctx.send("Could not shut down bot")
             print(e)
-            
-    @commands.command(name = "error_test", usage = "", description = "Creates an artifical error that will be logged")
+
+    @commands.group(name = "utils")
     @commands.is_owner()
+    async def utils(self, ctx) :
+        pass
+
+    @utils.command(name = "config_test", usage = "", description = "Tests the config plugin")
+    async def conftest(self, ctx) :
+        """
+        Tests Arusu's config
+        """
+        try :
+            await ctx.send(self.config.DATA[f"{ctx.guild.id}.Base"])
+        except :
+            self.config.update(f"{ctx.guild.id}.Base", "Arusu's config working")
+            await ctx.send(self.config.DATA[f"{ctx.guild.id}.Base"])
+
+    @utils.command(name = "embed_colour", usage = "", description = "Changes the embed default color")
+    async def embed_colour_set(self, ctx, colour = "#1900ff") :
+        self.config.update("BOT_EMBED_COLOUR", colour)
+        try :
+            embed = discord.Embed(title="Default Embed", description=f"Colour changed to {colour}", color=discord.Color.from_str(colour))
+            await ctx.send(embed = embed)
+        except : 
+            await ctx.send("Invalid colour")
+
+    @utils.command(name = "showsettings", usage = "", description = "Shows Arusu's config")
+    async def showset(self, ctx) : 
+        """
+        Show Arusu's config
+        """
+        try :
+            Settings = ""
+            try :
+                for i in self.config.DATA.keys() :
+                    if i != "BOT_TOKEN" :
+                        Settings += f"**{i}** : {self.config.DATA[i]} \n"
+            except Exception as e :
+                print("Error : error in Settings generation")
+                print(e)
+
+            embed = (discord.Embed(title="Settings",
+                                description=Settings,
+                                color = discord.Color.from_str(self.config.DATA["BOT_EMBED_COLOUR"])))
+        except Exception as e :
+            print("Error : error in embed settings generation")
+            print(e)
+        await ctx.send(embed = embed)
+
+    @utils.command(name = "prefix", usage = "[prefix]", description = "Changes Arusu's prefix, requires a restart to take effect")
+    async def prefixset(self, ctx, NEWPREFIX) :
+        try :
+            self.config.update("BOT_PREFIX", str(NEWPREFIX))
+            await ctx.send(f"Prefix changed to {NEWPREFIX}")
+        except Exception as e :
+            await ctx.send("Could not change prefix")
+            print("Error in changing prefix")
+            print(e)
+
+    @utils.command(name = "error_test", usage = "", description = "Creates an artifical error that will be logged")
     async def ErrorTest(self, ctx) :
         """
         Creates an artifical error that will be logged
