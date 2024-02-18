41fd824ef202210714823bd2735f7c22fe7b3e48
CommandoPAX <bruant.theo@gmail.com>
2024-02-18 20:58:52 +0100

    Arusuuuuu

--- Cogs/playlist.py
+++ Cogs/playlist.py
@@ -38,6 +38,37 @@ class Playlist(commands.Cog):
         self.CogName = "Playlist"
         self.config = ArusuConfig()
         
+    #Embed for the card list
+    def create_embed_list(self, Playlist_ : dict, page : int = 1):
+        
+        self.items_per_page = 20
+        self.pages = math.ceil(int(len(Playlist_["music_list"])) / self.items_per_page)
+
+        start = (page - 1) * self.items_per_page
+        end = start + self.items_per_page
+        
+        try :
+            List = ""
+            j = 1
+            try :
+                for i, link in enumerate(Playlist_[start:end], start=start):
+                    List += f"**{j}.** {link} \n"
+            except Exception as e :
+                LogError(CogName=self.CogName, CogFunct="create_embed_list", Error=e)
+
+            embed = (discord.Embed(title="Deck des catastrophes",
+                                description=List,
+                                color = discord.Color.from_str(self.config.DATA["BOT_EMBED_COLOUR"])).set_footer(text=f"Page {page}/{self.pages}"))
+            
+            embed_list = []
+            for i in range(self.pages) :
+                Emb = self.create_embed_list(page=i+1)
+                embed_list.append(Emb)
+            return embed_list
+            
+        except Exception as e :
+            LogError(CogName=self.CogName, CogFunct="create_embed_list", Error=e)
+        
     @commands.command(name = "playlist_init", usage = "", description = "Creates the required files for the cog to work")
     @commands.has_permissions(manage_guild = True)
     async def playlist_init(self, ctx) :
@@ -50,6 +81,9 @@ class Playlist(commands.Cog):
     
     @commands.command(name = "add", usage = "[youtube link]", description ="Adds a youtube link to a playlist")
     async def playlist_append(self, ctx, link) : 
+        """ 
+        Adds a youtube link to a playlist
+        """
         try :
             if ctx.author.id not in self.config["Playlist_ID"] :
                 file_manager.mk_custom(self.CogName, ctx.author.id)
@@ -60,8 +94,36 @@ class Playlist(commands.Cog):
             file_manager.update_custom(self.CogName, ctx.author.id, playlist_)
             await ctx.reply(f"{link} has been added to the playlist")
         except Exception as e :
-            LogError(CogName=self.CogName, CogFunct="guild_get_icon", Error=e)
-            await ErrorEmbed(ctx, Error=e, CustomMSG= "Error sending the guild icon")
+            LogError(CogName=self.CogName, CogFunct="playlist_append", Error=e)
+            await ErrorEmbed(ctx, Error=e, CustomMSG= "Error adding a music to the playlist")
+            
+    @commands.command(name = "remove", usage = "[youtube link]", description ="Removes a youtube link to a playlist")
+    async def playlist_remove(self, ctx, link) : 
+        """ 
+        Removes a youtube link to a playlist
+        """
+        try :
+            if ctx.author.id not in self.config["Playlist_ID"] :
+                await ctx.reply("You don't have any playlist")
+                return
+            playlist_ = file_manager.load_custom(self.CogName, ctx.author.id)
+            if link not in playlist_["music_list"] : 
+                await ctx.reply("Couldn't find the corresponding track")
+                return
+            playlist_["music_list"] = playlist_["music_list"].remove(link)
+            file_manager.update_custom(self.CogName, ctx.author.id, playlist_)
+            await ctx.reply(f"{link} has been removed from the playlist")
+        except Exception as e :
+            LogError(CogName=self.CogName, CogFunct="playlist_remove", Error=e)
+            await ErrorEmbed(ctx, Error=e, CustomMSG= "Error removing the music from the playlist")
+            
+    @commands.command(name = "list", usgae = "", description = "Lists all music from a playlist")
+    async def playlist_list(self, ctx) : 
+        """ 
+        Not currently functionnal
+        """
+        for element in self.Deck.list() :
+            await ctx.send(embed = element)
         
 async def setup(bot):
     await bot.add_cog(Playlist(bot))
